7369532ae537207383d30d983af3007610347b58
Matthew Zimmer <M16r01@gmail.com>
2016-11-11 22:44:42 -0500

    Added link to reduce_sum documentation for future reference when I review my notes.

--- section_notes/19_quiz_tensorflow_cross_entropy/reduce_sum.py
+++ section_notes/19_quiz_tensorflow_cross_entropy/reduce_sum.py
@@ -1,5 +1,9 @@
 import tensorflow as tf
 
+# The tf.reduce_sum() function takes an array of numbers and sums them together.
+#
+#   https://www.tensorflow.org/api_docs/python/math_ops.html#reduce_sum
+#
 x = tf.reduce_sum([1, 2, 3, 4, 5])
 
 tf.initialize_all_variables()
